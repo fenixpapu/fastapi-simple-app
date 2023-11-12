f0ebd62cd457307a9a482477cecda7b0ca05749b
PaPu <phucluongngoc@gmail.com>
2023-11-12 15:04:59 +0700

    add pod_name in response

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import os
 from fastapi import FastAPI
+
+POD_NAME = os.environ['POD_NAME']
 
 # Create an instance of the FastAPI framework
 app = FastAPI()
 
 # Define a route that returns "Hello, World!"
+
+
 @app.get("/")
 def read_root():
-    return {"message": "Hello, World! from v1"}
+    return {
+        "message": "Hello, World! from v3",
+        "pod_name": POD_NAME
+    }
