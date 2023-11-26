2a9ee975942a1aff1a093cb0d72d27bcf6ecb15d
PaPu <phucluongngoc@gmail.com>
2023-11-26 17:44:40 +0700

    add log to debug fluentd

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ POD_NAME = os.environ['POD_NAME']
 
 # Create an instance of the FastAPI framework
 app = FastAPI()
-print("{} - {}".format(POD_NAME, datetime.now().isoformat()))
+# print("{} - {}".format(POD_NAME, datetime.now().isoformat()))
 
 
 @app.get("/")
 def read_root():
+    print("Requested on: {} at: {}".format(
+        POD_NAME, datetime.now().isoformat()))
     return {
         "message": "Hello, World! from v3",
         "pod_name": POD_NAME
